f52cb1e6ed91fbcff6b59b6ba81897c13ba3cebf
Samantha Crespo <samkcrespo@gmail.com>
2022-04-05 22:41:22 -0700

    commit tree 06e498f31122658ec5c0b32cf39ecd952e232f1f

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat () {
+cats.push("Ralph");
+}
+
+function destructivelyPrependCat () {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift ();
+}
+function appendCat () {
+    const allCats = [...cats, "Broom"];
+    return allCats;
+}
+
+function prependCat () {
+    const allCats = ["Arnold", ...cats];
+    return allCats;
+}
+
+function removeLastCat () {
+    return cats.slice(0, cats.length-1);
+}
+function removeFirstCat () {
+    return cats.slice(1);
+}// Write your solution here!
